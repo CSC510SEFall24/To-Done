e59f443c1768117fcadbb9d3aa6ec8e278bba895
Vyom Patel <vyompatel7188@gmail.com>
2024-11-25 01:45:41 -0500

    Updated models.py to add set priority feature

--- todo/models.py
+++ todo/models.py
@@ -51,6 +51,12 @@ class ListTags(models.Model):
         return "%s" % self.tag_name
 
 
+PRIORITY_CHOICES = [
+    ('High', 'High'),
+    ('Medium', 'Medium'),
+    ('Low', 'Low'),
+]
+
 class ListItem(models.Model):
     # the name of a list item
     item_name = models.CharField(max_length=50, null=True, blank=True)
@@ -63,6 +69,12 @@ class ListItem(models.Model):
     finished_on = models.DateTimeField()
     due_date = models.DateField()
     tag_color = models.CharField(max_length=10)
+    # Add this new field
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default='Low',  # Default priority
+    )
 
     objects = models.Manager()
 
